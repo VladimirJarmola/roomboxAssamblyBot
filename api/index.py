dac2b0de2ad0128cfc8f7c12645d251e43e660e4
Vladimir Jarmola <vlad.jarmola@gmail.com>
2024-06-25 19:43:04 +0600

    fastapi_3

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
+
 from app import bot
 
 app = FastAPI()
 
-@app.post('/api/bot')
+@app.post('/')
 async def tgbot_webhook_route(request: Request):
     update_dict = await request.json()
     await bot.update_bot(update_dict)
